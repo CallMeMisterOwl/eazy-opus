a9febadb8f2e0bf3b926083acc6c2409e86cac86
Deniz Enes Hasler <deniz.hasler@titanom.com>
2025-03-11 09:24:52 +0100

    fix: revert page.tsx

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -3,8 +3,7 @@ import { taskTable } from '@/app/db/schema';
 import { eq } from 'drizzle-orm';
 import { TaskList } from './task-list';
 
-export default async function Project({ params }: { params: { id: number } }) {
-  const { id } = await params;
+export default async function Project({ params: { id } }: { params: { id: number } }) {
   const tasks = await db.select().from(taskTable).where(eq(taskTable.projectID, id));
   return <TaskList projectID={id} tasks={tasks}/>;
   
