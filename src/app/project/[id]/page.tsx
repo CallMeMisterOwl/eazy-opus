34f95e79d05f5bd74cf6e333d9c8edc44ab10c7a
Deniz Enes Hasler <deniz.hasler@titanom.com>
2025-03-11 10:51:13 +0100

    fix: await params

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -3,8 +3,20 @@ import { taskTable } from '@/app/db/schema';
 import { eq } from 'drizzle-orm';
 import { TaskList } from './task-list';
 
-export default async function Project({ params: { id } }: { params: { id: number } }) {
-  const tasks = await db.select().from(taskTable).where(eq(taskTable.projectID, id));
-  return <TaskList projectID={id} tasks={tasks}/>;
+type Params = Promise<{ id: string }>
+
+export default async function Project(props: { params: Params }) {
+  const params = await props.params;
+  const id = params.id;
+  console.log("Raw Params:", params); // Debugging
+  const int_id = Number(id)
+  console.log("Converted Project ID:", id);
+
+  if (isNaN(int_id)) {
+    throw new Error("Invalid Project ID");
+  }
+  const tasks = await db.select().from(taskTable).where(eq(taskTable.projectID, int_id));
+  console.log("Fetched Tasks:", tasks);
+  return <TaskList projectID={int_id} tasks={tasks}/>;
   
 }
