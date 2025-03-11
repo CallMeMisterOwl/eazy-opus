636f9b1d9c35b5dfd4a119283d6bae75537351a4
Deniz Enes Hasler <deniz.hasler@titanom.com>
2025-03-11 09:21:21 +0100

    chore: format

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -3,7 +3,7 @@ import { taskTable } from '@/app/db/schema';
 import { eq } from 'drizzle-orm';
 import { TaskList } from './task-list';
 
-export default async function Project({ params}: { params: { id: number } }) {
+export default async function Project({ params }: { params: { id: number } }) {
   const { id } = await params;
   const tasks = await db.select().from(taskTable).where(eq(taskTable.projectID, id));
   return <TaskList projectID={id} tasks={tasks}/>;
