39efa0704d34f9f55e724d0bc454dccbb87753b8
Deniz Enes Hasler <deniz.hasler@titanom.com>
2025-03-11 10:54:58 +0100

    fix: vercal typeerror

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -8,15 +8,9 @@ type Params = Promise<{ id: string }>
 export default async function Project(props: { params: Params }) {
   const params = await props.params;
   const id = params.id;
-  console.log("Raw Params:", params); // Debugging
   const int_id = Number(id)
-  console.log("Converted Project ID:", id);
-
-  if (isNaN(int_id)) {
-    throw new Error("Invalid Project ID");
-  }
+  
   const tasks = await db.select().from(taskTable).where(eq(taskTable.projectID, int_id));
-  console.log("Fetched Tasks:", tasks);
   return <TaskList projectID={int_id} tasks={tasks}/>;
   
 }
